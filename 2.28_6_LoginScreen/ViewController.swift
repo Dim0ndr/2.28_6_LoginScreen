4acbf5961fad93690ea491837cd5e9544d8c376c
Dim0ndr <dimondr@gmail.com>
2022-07-03 21:46:13 +0300

    Initial Commit

---
+++ 2.28_6_LoginScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.28_6_LoginScreen
+//
+//  Created by Dimondr on 03.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
